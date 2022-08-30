29535239122cd2cbbc32b36050013ce694971fa3
Martin Ehrnst <martin.ehrnst@vipps.no>
2022-08-30 14:53:04 +0200

    ..

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
-import { azureResourcesPlugin, AzureResourcesPage } from '../src/plugin';
+import { azureResourcesPlugin } from '../src/plugin';
 
 createDevApp()
   .registerPlugin(azureResourcesPlugin)
-  .addPage({
-    element: <AzureResourcesPage />,
-    title: 'Root Page',
-    path: '/azure-resources'
-  })
   .render();
