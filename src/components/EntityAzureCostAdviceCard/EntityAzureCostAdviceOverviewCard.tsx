ae9efc9f73c528be4066f72c2da113f1cb8bb6f3
tinolazreg <tino.lazreg@vipps.no>
2022-12-14 15:05:41 +0100

    Add impact

--- src/components/EntityAzureCostAdviceCard/EntityAzureCostAdviceOverviewCard.tsx
+++ src/components/EntityAzureCostAdviceCard/EntityAzureCostAdviceOverviewCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { InfoCard } from "@backstage/core-components";
-import { GetEntityAzureCostAdvice } from "../EntityFetchAzureData";
+import { GetEntityAzureResourceGroups } from "../EntityFetchAzureData";
 
 
 export const EntityAzureCostAdviceCard = () => {
@@ -10,7 +10,7 @@ export const EntityAzureCostAdviceCard = () => {
             <Grid item>
                 <InfoCard>
                     <Typography variant="body2">
-                        <GetEntityAzureCostAdvice />
+                        <GetEntityAzureResourceGroups />
                     </Typography>
                 </InfoCard>
             </Grid>
